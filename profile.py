bf1912fa981a82d15a80e196cb5d516bfc7f812f
tal7aouy <tal7aouy.dev@gmail.com>
2025-01-15 17:50:39 +0100

    edit

--- profile.py
+++ profile.py
@@ -9,20 +9,17 @@ console = Console(record=True, width=110)
 
 # Expertise Tree
 tree = Tree("🤓 [link=https://talhaouy.me]Mhammed Talhaouy", guide_style="bold cyan")
-expertise_tree = tree.add("💻 Sr Software Engineer | Web3 & Security Enthusiast", guide_style="bold green")
-expertise_tree.add("🐘 PHP | Laravel Specialist")
-expertise_tree.add("🚀 Node.js | Vue.js")
+expertise_tree = tree.add("💻 Software Engineer | Web3 Enthusiast", guide_style="bold green")
+expertise_tree.add("🐘 PHP | Rust | Python | Js | Solidity")
+expertise_tree.add("🚀 Laravel | Node.js | Vue.js")
 expertise_tree.add("🧠 LangChain | PyTorch | Pandas | NumPy")
-expertise_tree.add("🛠️ Solidity | Smart Contracts")
 expertise_tree.add("☁️ AWS | CI/CD Pipelines | Nginx")
 expertise_tree.add("📦 MySQL | Docker | Redis")
 tree.add("🧹 Clean Coder | 🕵️ Auditor")
 
 # About Panel
 about_content = """
-I'm a passionate software engineer driven by innovation, security, and Web3.  
-I build tools to strengthen systems without disrupting what supports them.
-
+I enjoy creating tools that break things to make systems stronger, but never break what helps build — that's just unfair.
 🌟 [green]Follow me on Twitter: [bold link=https://twitter.com/tal7aouy]@tal7aouy[/]
 """
 
@@ -31,7 +28,7 @@ about_panel = Panel.fit(
     box=box.DOUBLE,
     border_style="blue",
     title="[b]👋 Welcome",
-    width=50,
+    width=60,
 )
 
 # Display Content
